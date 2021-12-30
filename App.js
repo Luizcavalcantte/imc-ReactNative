42d87fa81622e1c2930b8265038f8388bab21c6b
Luiz Cavalcante <85976619+Luizcavalcantte@users.noreply.github.com>
2021-12-30 13:38:39 -0300

    Update App.js

--- App.js
+++ App.js
@@ -46,7 +46,6 @@ export default function App() {
       <TextInput
         placeholder="Altura"
         placeholderTextColor="lightgray"
-        returnKeyType="numeric"
         keyboardType="numeric"
         style={styles.input}
         onChangeText={inputAltura => {
